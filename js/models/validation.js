7acd8a6fd45cec4e78ce9bc112a0aca28194f98c
Matt Fenwick <mfenwick100@gmail.com>
2013-11-29 15:49:47 -0500

    get rid of unnecessary module, folding it into another module that it was tightly coupled to

--- js/models/validation.js
+++ js/models/validation.js
@@ -1,15 +1,37 @@
 define([
-    'parser/full'
-], function (Parser) {
+    'parser/json',
+    'parser/jsontree'
+], function (J, JT) {
     'use strict';
 
     function V() {
         this.listeners = [];
     }
     
+    V.prototype.parse_helper = function(input) {
+        var parsed = J.json.parse(input, [1, 1]);
+        if ( parsed.status !== 'success' ) {
+            return {
+                'stage': 'cst',
+                'value': parsed.value
+            };
+        }
+        if ( parsed.value.rest.length !== 0 ) { // shouldn't happen
+            throw new Error('unparsed input remaining at line ' + st[0] + ', column ' + st[1]);
+        }
+        var valids = JT.t_json(parsed.value.result);
+        if ( valids.errors.length > 0 ) {
+            return {
+                'stage': 'ast',
+                'value': valids.errors
+            };
+        }
+        return {'stage': 'done', 'value': 'success'};
+    }
+    
     V.prototype.parse = function(input) {
         try {
-            var parsed = Parser.parse(input, [1, 1]);
+            var parsed = this.parse_helper(input);
             this.notify(parsed);
         } catch (e) {
             this.notify({'stage': 'unexerr', 'message': e.message});
