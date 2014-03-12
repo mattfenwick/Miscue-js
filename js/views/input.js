855c0262f8168e83a48506b6ffe418ed6aa4a2ed
Matt Fenwick <mfenwick100@gmail.com>
2014-03-12 08:05:09 -0400

    add visual cue when input is changed but hasn't yet been submitted

--- js/views/input.js
+++ js/views/input.js
@@ -4,6 +4,11 @@ define(function () {
     function Input(button, elem, model) {
         button.click(function() {
             model.parse(elem.val());
+            elem.removeClass('changed');
+        });
+        
+        elem.on('input', function() {
+            elem.addClass('changed');
         });
     }
 
